562c8f6194e6d129bc44e768aa72604f26c1911a
Yannic Beyer <y.beyer@tu-bs.de>
2023-08-08 17:40:59 +0200

    Update license

--- scripts/plot_rotor_failure_pos_tracking.m
+++ scripts/plot_rotor_failure_pos_tracking.m
@@ -2,7 +2,7 @@
 % total rotor failure in simulation and flight test.
 
 % Disclamer:
-%   SPDX-License-Identifier: GPL-2.0-only
+%   SPDX-License-Identifier: GPL-3.0-only
 % 
 % 	Copyright (C) 2022-2023 Yannic Beyer
 %   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
